daf217ddc09db56b5e7baf352f534e8a2a9b7b5d
Benedikt Erhart <contact@b-erhart.de>
2023-07-23 23:08:57 +0200

    Add vector

---
+++ geometry/vector.go
@@ -0,0 +1,58 @@
+package geometry
+
+import (
+	"fmt"
+	"math"
+)
+
+// A 3-dimensional vector.
+type Vector struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+// Get the euclidian length of a vector.
+func (v *Vector) Length() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z)
+}
+
+// Get the normalized variant of a vector.
+func (v *Vector) Normalize() *Vector {
+	normFactor := 1 / v.Length()
+	return Sprod(v, normFactor)
+}
+
+// Add two vectors.
+func Add(a, b *Vector) *Vector {
+	return &Vector{a.X + b.X, a.Y + b.Y, a.Z + b.Z}
+}
+
+// Subtract the second vector from the first vector.
+func Sub(a, b *Vector) *Vector {
+	return &Vector{a.X - b.X, a.Y - b.Y, a.Z - b.Z}
+}
+
+// Get the cross product of two vectors.
+func Cross(a, b *Vector) *Vector {
+	return &Vector{
+		a.Y*b.Z - b.Y*a.Z,
+		b.X*a.Z - a.X*b.Z,
+		a.X*b.Y - b.X*a.Y,
+	}
+}
+
+// Get the dot product of two vectors.
+func Dot(a, b *Vector) float64 {
+	return a.X*b.X + a.Y*b.Y + a.Z*b.Z
+}
+
+// Get the product of a vector and a scalar.
+func Sprod(v *Vector, s float64) *Vector {
+	return &Vector{s * v.X, s * v.Y, s * v.Z}
+}
+
+// Get the string representation of a vector.
+func (v Vector) String() string {
+	return fmt.Sprintf("(%f, %f, %f)", v.X, v.Y, v.Z)
+}
--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 
 	for j := 0; j < 10; j++ {
 		for i := 0; i < canvas.Width(); i++ {
-			if i < canvas.Width() / 2 {
+			if i < canvas.Width()/2 {
 				canvas.SetPixel(i, j, 0, 0, 0)
 			} else {
 				canvas.SetPixel(i, j, 255, 255, 255)
@@ -23,15 +23,15 @@ func main() {
 	for j := 10; j < canvas.Height(); j++ {
 		for i := 0; i < canvas.Width(); i++ {
 			switch {
-			case i < 1 * canvas.Width()/6:
+			case i < 1*canvas.Width()/6:
 				canvas.SetPixel(i, j, 115, 11, 219)
-			case i < 2 * canvas.Width()/6:
+			case i < 2*canvas.Width()/6:
 				canvas.SetPixel(i, j, 11, 105, 219)
-			case i < 3 * canvas.Width()/6:
+			case i < 3*canvas.Width()/6:
 				canvas.SetPixel(i, j, 54, 138, 12)
-			case i < 4 * canvas.Width()/6:
+			case i < 4*canvas.Width()/6:
 				canvas.SetPixel(i, j, 222, 212, 16)
-			case i < 5 * canvas.Width()/6:
+			case i < 5*canvas.Width()/6:
 				canvas.SetPixel(i, j, 222, 129, 16)
 			default:
 				canvas.SetPixel(i, j, 222, 16, 16)
