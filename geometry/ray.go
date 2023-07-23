949a8d69350efed7bd52c206d1cd99727c7becb1
Benedikt Erhart <contact@b-erhart.de>
2023-07-23 23:45:41 +0200

    Add String() method to ray

--- geometry/ray.go
+++ geometry/ray.go
@@ -1,7 +1,13 @@
 package geometry
 
+import "fmt"
+
 type Ray struct {
 	Origin    *Vector
 	Direction *Vector
 	Depth     int
+}
+
+func (r Ray) String() string {
+	return fmt.Sprintf("%v + %d*%v", r.Origin, r.Depth, r.Direction)
 }
