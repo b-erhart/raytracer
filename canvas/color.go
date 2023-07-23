7e7ef2274c569a67d2425375405c5da401e573ea
Benedikt Erhart <contact@b-erhart.de>
2023-07-23 23:29:19 +0200

    Add String() method to color

--- canvas/color.go
+++ canvas/color.go
@@ -1,8 +1,15 @@
 package canvas
 
+import "fmt"
+
 // RGB color.
 type Color struct {
 	R uint8
 	G uint8
 	B uint8
+}
+
+// Get string representation of color.
+func (c Color) String() string {
+	return fmt.Sprintf("(%3d, %3d, %3d)", c.R, c.G, c.B)
 }
